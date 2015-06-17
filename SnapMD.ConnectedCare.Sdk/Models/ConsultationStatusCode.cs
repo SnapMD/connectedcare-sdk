10a8d60b1bcc3613421c323cda48ef1beaaaa35a
Aaron Lord <aaron@lorddesign.net>
2015-06-17 09:59:43 -0700

    Add status codes

---
+++ SnapMD.ConnectedCare.Sdk/Models/ConsultationStatusCode.cs
@@ -0,0 +1,14 @@
+﻿namespace SnapMD.ConnectedCare.Sdk.Models
+{
+    public enum ConsultationStatusCode
+    {
+        PaymentDone = 68,
+        DoctorAssigned = 69,
+        CustomerInWaiting=82,
+        DoctorInitiatedConsultation = 70,
+        StartedConsultation = 71,
+        EndedConsultation = 72,
+        CancelConsultaion = 79,
+        InProgress = 80
+    };
+}
