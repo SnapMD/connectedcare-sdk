dd1e8efb78373b8021b6d6adc78d3351792937bb
Aaron Lord <aaron@lorddesign.net>
2015-07-10 14:44:10 -0700

    cleanup

---
+++ SnapMD.ConnectedCare.Sdk/Models/CodeSetResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.ConnectedCare.Sdk.Models
+{
+    public class CodeSetResponse
+    {
+        public string Name { get; set; }
+        public int HospitalId { get; set; }
+        public IList<CodeItem> Codes { get; set; }
+
+        public class CodeItem
+        {
+            public int CodeId { get; set; }
+            public string Text { get; set; }
+            public int DisplayOrder { get; set; }
+        }
+    }
+}
