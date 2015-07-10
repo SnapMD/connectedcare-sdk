a9244ed90acc05fe85824e91cfe39b37503ea76e
Aaron Lord <aaron@lorddesign.net>
2015-07-10 16:58:56 -0700

    Change folder paths

--- SnapMD.ConnectedCare.Sdk.Tests/Properties/Settings.Designer.cs
+++ SnapMD.ConnectedCare.Sdk.Tests/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SnapMD.ConnectedCare.Sdk.Test.Properties {
+namespace SnapMD.ConnectedCare.Sdk.Tests.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
