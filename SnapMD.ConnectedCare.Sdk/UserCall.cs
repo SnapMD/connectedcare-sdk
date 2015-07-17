3c5b532a0af92a71f7c0383fd66b82d470bd0d8a
Sameer Siddiqui <sameerfairgoogl@gmail.com>
2015-07-18 00:12:32 +0500

    Fir for UserAPI Path issue

--- SnapMD.ConnectedCare.Sdk/UserCall.cs
+++ SnapMD.ConnectedCare.Sdk/UserCall.cs
@@ -10,6 +10,7 @@
 //    limitations under the License.
 using System;
 using SnapMD.ConnectedCare.Sdk.Models;
+//using SnapMD.ConnectedCare.ApiModels;
 using Newtonsoft.Json.Linq;
 
 
@@ -26,7 +27,7 @@ namespace SnapMD.ConnectedCare.Sdk
         {
             //var baseUrl = new Uri(_baseUrl);
             //var url = new Uri(baseUrl, "account/user");
-            var o = MakeCall("account/userv2");
+            var o = MakeCall("v2/account/user");
 
             //userId = Convert.ToInt32(o["id"]);
 
