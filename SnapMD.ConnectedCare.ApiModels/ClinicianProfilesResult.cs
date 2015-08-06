b22fc2ebd06d878476842a23360195fd5fbe59b9
Aaron Lord <aaron@lorddesign.net>
2015-08-06 11:19:25 -0700

    Moving SDK models

---
+++ SnapMD.ConnectedCare.ApiModels/ClinicianProfilesResult.cs
@@ -0,0 +1,51 @@
+﻿namespace SnapMD.ConnectedCare.ApiModels
+{
+    /// <summary>
+    ///     Represents public information for a clinician that is suitable for patient use.
+    /// </summary>
+    public class ClinicianProfilesResult
+    {
+        public string BusinessAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public string Gender { get; set; }
+
+        public int HospitalId { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MedicalLicense { get; set; }
+
+        public string MedicalSpeciality { get; set; }
+
+        public string MedicalSchool { get; set; }
+
+        /// <summary>
+        ///     Get Doctor profile image or default one.
+        /// </summary>
+        public string ProfileImage
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(ProfileImagePath))
+                {
+                    return ProfileImagePath;
+                }
+
+                return Gender == "F" ? "/images/Doctor-Female.gif" : "/images/Doctor-Male.gif";
+            }
+        }
+
+        public string ProfileImagePath { get; set; }
+
+        public string StatesLicenced { get; set; }
+
+        public string SubSpeciality { get; set; }
+    }
+}
---
+++ SnapMD.ConnectedCare.ApiModels/PatientConsultationInfo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace SnapMD.ConnectedCare.ApiModels
+{
+    public class PatientConsultationInfo
+    {
+        public int ConsultationId { get; set; }
+        public int PatientId { get; set; }
+        public int ConsultantUserId { get; set; }
+        public int AssignedDoctorId { get; set; }
+        public string AssignedDoctorFirstName { get; set; }
+        public string AssignedDoctorLastName { get; set; }
+
+        public string AssignedDoctorName
+        {
+            get { return string.Format("{0} {1}", AssignedDoctorFirstName, AssignedDoctorLastName).Trim(); }
+        }
+
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+
+        public string PatientName
+        {
+            get { return string.Format("{0} {1}", PatientFirstName, PatientLastName).Trim(); }
+        }
+
+        public string GuardianFirstName { get; set; }
+
+        public string Guardianame
+        {
+            get { return string.Format("{0} {1}", GuardianFirstName, GuardianLastName); }
+        }
+
+        public string GuardianLastName { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public string ConsultationDate
+        {
+            get
+            {
+                // ConsultationTimeInfo time has valid date time (converted from TimeZone because it has both date and time)
+                if (ConsultationTimeInfo.HasValue)
+                {
+                    return ConsultationTimeInfo.Value.ToString("MMM dd, yyyy");
+                }
+                return string.Empty;
+            }
+        }
+
+        public DateTime? ConsultationTimeInfo { get; set; }
+
+        public string ConsultationTime
+        {
+            get
+            {
+                if (ConsultationTimeInfo.HasValue)
+                {
+                    return ConsultationTimeInfo.Value.ToString(@"hh\:mm tt");
+                }
+                return string.Empty;
+            }
+        }
+
+        public DateTime? WaitingConsultationInfo { get; set; }
+        public DateTime? StartedConsultationInfo { get; set; }
+
+        public string WaitingConsultation
+        {
+            get
+            {
+                if (WaitingConsultationInfo.HasValue)
+                {
+                    return WaitingConsultationInfo.Value.ToString("MMM dd, yyyy");
+                }
+                return string.Empty;
+            }
+        }
+
+        public string StartedConsultation
+        {
+            get
+            {
+                if (StartedConsultationInfo.HasValue)
+                {
+                    return StartedConsultationInfo.Value.ToString(@"hh\:mm");
+                }
+                return string.Empty;
+            }
+        }
+
+        public long? ConsultationDuration { get; set; }
+    }
+}
