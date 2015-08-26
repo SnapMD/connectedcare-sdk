29c4751af9561261716b6fe216688a0e7223631a
chris camacho <dev.chriscamacho@gmail.com>
2015-08-27 00:17:29 +0800

    added missed files

---
+++ SnapMD.ConnectedCare.ApiModels/HospitalHours.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.ConnectedCare.ApiModels
+{
+    public struct HospitalHours
+    {
+        public int StartDayOfWeek { get; set; }
+        public int EndDayOfWeek { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
