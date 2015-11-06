21261dce23bba788157b0f607c05d81723ef9c70
Aaron Lord <aaron@lorddesign.net>
2015-11-06 15:29:56 -0800

    Increment assembly versions

--- SnapMD.ConnectedCare.Sdk/Properties/AssemblyInfo.cs
+++ SnapMD.ConnectedCare.Sdk/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.16.0.*")]
+[assembly: AssemblyVersion("1.17.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
