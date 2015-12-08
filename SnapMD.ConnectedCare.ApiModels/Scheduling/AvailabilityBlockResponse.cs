7f6ff7a03a967a12f7cabb90bda48cb7c68ac383
Aaron Lord <aaron@lorddesign.net>
2015-12-07 20:23:28 -0800

    Begin models and bitwise calculations re #4799

---
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlockResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnapMD.ConnectedCare.ApiModels.Scheduling
+{
+    public class AvailabilityBlockResponse : AvailabilityBlockRequest
+    {
+        public int CreatedByUserId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int Id { get; set; }
+
+        public int? ModifiedByUserId { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public AvailabilityBlockRuleResponse Rule { get; set; }
+    }
+}
