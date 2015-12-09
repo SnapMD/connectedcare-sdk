78a0ffcdfa227a174024970299d73285caf28138
Aaron Lord <aaron@lorddesign.net>
2015-12-08 16:36:28 -0800

    Finish models re #4799

---
+++ SnapMD.ConnectedCare.ApiModels/AppointmentStatusCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SnapMD.ConnectedCare.ApiModels
+{
+    [Flags]
+    public enum AppointmentStatusCode
+    {
+        //0 = none
+        //1 = pending
+        //2 = scheduled
+        //4 = active
+        //8 = canceled
+        //16 = completed
+
+        None = 0,
+        Pending = 1,
+        Scheduled = 1 << 1,
+        Active = 1 << 2,
+        Canceled = 1 << 3,
+        Completed = 1 << 4
+    }
+}
---
+++ SnapMD.ConnectedCare.ApiModels/GlobalStatusCode.cs
@@ -0,0 +1,13 @@
+﻿namespace SnapMD.ConnectedCare.ApiModels
+{
+    /// <summary>
+    /// Requested by daniel.bouganim@snap.md
+    /// </summary>
+    public enum GlobalStatusCode
+    {
+        None,
+        Active,
+        Deleted,
+        Suspended
+    }
+}
