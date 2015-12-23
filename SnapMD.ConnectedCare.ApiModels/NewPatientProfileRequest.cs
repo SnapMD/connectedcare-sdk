4221244f888f25c96612554ba5886a6d821d7eda
Jay Lee <jay.lee@ji-technology.com>
2015-12-22 17:24:47 -0800

    add classes to support AddDependentForPatient

---
+++ SnapMD.ConnectedCare.ApiModels/NewPatientProfileRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.ConnectedCare.ApiModels
+{
+    public class NewPatientProfileRequest
+    {
+        public string EmailAddress { get; set; }
+        public string TimeZoneId { get; set; }
+        public PatientProfileRequest PatientProfileData { get; set; }
+        public PatientMedicalHistoryRequest PatientMedicalHistoryData { get; set; }
+        public PatientProfileFieldsTracingRequest PatientProfileFieldsTracing { get; set; }
+    }
+}
---
+++ SnapMD.ConnectedCare.ApiModels/PatientMedicalHistoryRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.ConnectedCare.ApiModels
+{
+    public class PatientMedicalHistoryRequest
+    {
+        public int HistoryId { get; set; }
+        public int PatientId { get; set; }
+        public int? MedicalCondition1 { get; set; }
+        public int? MedicalCondition2 { get; set; }
+        public int? MedicalCondition3 { get; set; }
+        public int? MedicalCondition4 { get; set; }
+        public string PriorSurgery1 { get; set; }
+        public string PriorSurgery2 { get; set; }
+        public string PriorSurgery3 { get; set; }
+        public int? Surgery1Month { get; set; }
+        public int? Surgery2Month { get; set; }
+        public int? Surgery3Month { get; set; }
+        public int? Surgery1Year { get; set; }
+        public int? Surgery2Year { get; set; }
+        public int? Surgery3Year { get; set; }
+        public int? AllergicMedication1 { get; set; }
+        public int? AllergicMedication2 { get; set; }
+        public int? AllergicMedication3 { get; set; }
+        public int? AllergicMedication4 { get; set; }
+        public int? TakingMedication1 { get; set; }
+        public int? TakingMedication2 { get; set; }
+        public int? TakingMedication3 { get; set; }
+        public string TakingMedication4 { get; set; }
+        public string IsVaccinationUpToDate { get; set; }
+        public string IsOneYearBelowChild { get; set; }
+        public string IsChildBornFullTerm { get; set; }
+        public string IsChildBornVaginally { get; set; }
+        public string IsChildDischargeMother { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
