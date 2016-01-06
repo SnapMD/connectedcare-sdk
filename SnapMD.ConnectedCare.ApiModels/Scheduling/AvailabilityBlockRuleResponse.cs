de3eace50ad88ad9bb70e465d8c64872cbce370d
Jay Lee <jay.lee@ji-technology.com>
2016-01-06 12:20:41 -0800

    AvailabilityBlocksResponse

--- SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlockRuleResponse.cs
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlockRuleResponse.cs
@@ -5,5 +5,6 @@ namespace SnapMD.ConnectedCare.ApiModels.Scheduling
     public class AvailabilityBlockRuleResponse : AvailabilityBlockRuleRequest
     {
         public int Id { get; set; }
+        public DateTime? ModifiedDate { get; set; }
     }
 }
---
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlocksRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.ConnectedCare.ApiModels.Scheduling
+{
+    public class AvailabilityBlocksRequest
+    {
+        public int[] clinicianIds;
+        public DateTime? startDate;
+        public DateTime? endDate;
+    }
+}
