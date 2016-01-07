b7bd164503f4ed798b6bc18a4e113b33e50279ba
Jay Lee <jay.lee@ji-technology.com>
2016-01-06 16:34:53 -0800

    clean up unused usings.

--- SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlocksRequest.cs
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlocksRequest.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnapMD.ConnectedCare.ApiModels.Scheduling
 {
     public class AvailabilityBlocksRequest
     {
-        public int[] clinicianIds;
-        public DateTime? startDate;
-        public DateTime? endDate;
+        public int[] ClinicianIds;
+        public DateTime? StartDate;
+        public DateTime? EndDate;
     }
 }
--- SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlocksResponse.cs
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/AvailabilityBlocksResponse.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnapMD.ConnectedCare.ApiModels.Scheduling
+﻿namespace SnapMD.ConnectedCare.ApiModels.Scheduling
 {
     public class AvailabilityBlocksResponse
     {
-        public AvailabilityBlockResponse[] availabilityBlocks;
-        public AvailabilityBlockRuleResponse[] availabilityBlockRules;
+        public AvailabilityBlockResponse[] AvailabilityBlocks;
+        public AvailabilityBlockRuleResponse[] AvailabilityBlockRules;
     }
 }
