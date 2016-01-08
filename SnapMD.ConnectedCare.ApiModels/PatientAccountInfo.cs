91e6a4680591741e7c1645de912233f87321b09f
Dan Parker <dan.parker@snap.md>
2016-01-08 14:20:32 -0800

    make isdependent a bool on patient profile

--- SnapMD.ConnectedCare.ApiModels/PatientAccountInfo.cs
+++ SnapMD.ConnectedCare.ApiModels/PatientAccountInfo.cs
@@ -25,7 +25,7 @@ namespace SnapMD.ConnectedCare.ApiModels
 
         public bool IsAuthorized { get; set; }
         public PatientAccountStatus Status { get; set; }
-        public string IsDependent { get; set; }
+        public bool IsDependent { get; set; }
         public int? GuardianId { get; set; }
         public string Email { get; set; }
     }
