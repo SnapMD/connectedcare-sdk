e5a6f7b9457504872f27e0137c3d7d56e89961d2
Andrei <andrei.mikhalevich>
2016-01-20 16:33:11 -0400

    #5191 new code enums added, appointment class added

---
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnapMD.ConnectedCare.ApiModels.Scheduling
+{
+    public class Appointment
+    {
+        public Guid AppointmentId { get; set; }
+        public int AvailabilityBlockId { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? AppointmentRequestId { get; set; }
+        public int EncounterMetadataId { get; set; }
+        public AppointmentStatusCode AppointmentStatusCode { get; set; }
+        public AppointmentTypeCode AppointmentTypeCode { get; set; }
+    }
+}
