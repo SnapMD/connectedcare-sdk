4ef1c8f5a540047b49d32d2758baa08bd5d0fc1d
Aaron <aaron@lorddesign.net>
2016-01-22 10:13:48 -0800

    Appointment block nullable

--- SnapMD.ConnectedCare.ApiModels/Scheduling/Appointment.cs
+++ SnapMD.ConnectedCare.ApiModels/Scheduling/Appointment.cs
@@ -5,10 +5,10 @@ namespace SnapMD.ConnectedCare.ApiModels.Scheduling
     public class Appointment
     {
         public Guid AppointmentId { get; set; }
-        public int AvailabilityBlockId { get; set; }
+        public int? AvailabilityBlockId { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
-        public int? AppointmentRequestId { get; set; }
+        public int? OnDemandRequestId { get; set; }
         public int EncounterMetadataId { get; set; }
         public AppointmentStatusCode AppointmentStatusCode { get; set; }
         public AppointmentTypeCode AppointmentTypeCode { get; set; }
