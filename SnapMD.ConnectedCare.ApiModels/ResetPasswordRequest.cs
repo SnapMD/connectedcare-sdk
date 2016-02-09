cb790f146b45f2b0fbccd4aff8bf01a7918f60b3
Jay Lee <jay.lee@ji-technology.com>
2016-02-08 17:20:36 -0800

    use UserTypeId to indicate which password to reset.

--- SnapMD.ConnectedCare.ApiModels/ResetPasswordRequest.cs
+++ SnapMD.ConnectedCare.ApiModels/ResetPasswordRequest.cs
@@ -1,9 +1,11 @@
-﻿namespace SnapMD.ConnectedCare.ApiModels
+﻿using SnapMD.ConnectedCare.ApiModels.Enums;
+
+namespace SnapMD.ConnectedCare.ApiModels
 {
     public class ResetPasswordRequest
     {
         public string Email { get; set; }
         public int HospitalId { get; set; }
-        public bool? Admin { get; set; }
+        public UserType? UserTypeId { get; set; }
     }
 }
