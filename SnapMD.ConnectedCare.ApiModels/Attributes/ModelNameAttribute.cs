d7f662cc91642ac8c81a43d15c408699c4802cc0
Aaron Lord <aaron@lorddesign.net>
2016-02-11 18:43:48 -0800

    cleanup

--- SnapMD.ConnectedCare.ApiModels/Attributes/ModelNameAttribute.cs
+++ SnapMD.ConnectedCare.ApiModels/Attributes/ModelNameAttribute.cs
@@ -6,7 +6,7 @@ namespace SnapMD.ConnectedCare.ApiModels.Attributes
     /// Use this attribute to change the name of the <see cref="ModelDescription"/> generated for a type.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
-    public sealed class ModelNameAttribute : Attribute
+    public class ModelNameAttribute : Attribute
     {
         public ModelNameAttribute(string name)
         {
