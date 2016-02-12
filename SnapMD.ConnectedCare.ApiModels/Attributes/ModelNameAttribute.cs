f31066ed3b29ef4a875d733fbb7d1b6d2553ddf4
Aaron Lord <aaron@lorddesign.net>
2016-02-11 19:13:28 -0800

    Documentation

--- SnapMD.ConnectedCare.ApiModels/Attributes/ModelNameAttribute.cs
+++ SnapMD.ConnectedCare.ApiModels/Attributes/ModelNameAttribute.cs
@@ -8,11 +8,21 @@ namespace SnapMD.ConnectedCare.ApiModels.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
     public class ModelNameAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelNameAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
         public ModelNameAttribute(string name)
         {
             Name = name;
         }
 
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; private set; }
     }
 }
