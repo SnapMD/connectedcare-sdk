1801992a83866cc9e26252493d421e1ef182a207
Jay Lee <jay.lee@ji-technology.com>
2016-02-15 17:47:51 -0800

    feat: request model for Email User

--- SnapMD.ConnectedCare.ApiModels/ResetPasswordRequest.cs
+++ SnapMD.ConnectedCare.ApiModels/ResetPasswordRequest.cs
@@ -2,6 +2,10 @@
 
 namespace SnapMD.ConnectedCare.ApiModels
 {
+    /// <summary>
+    /// Request Model for sending Reset Password Email.
+    /// </summary>
+    /// <seealso cref="SnapMD.ConnectedCare.ApiModels.EmailUserRequest" />
     public class ResetPassWordRequest : EmailUserRequest
     {
     }
