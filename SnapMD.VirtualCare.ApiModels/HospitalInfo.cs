59d9b44fe5d28b121e16bbdc25d95e9579ca184f
Aaron Lord <aaron@lorddesign.net>
2016-02-18 21:59:43 -0800

    Add models for Provider APIs

--- SnapMD.VirtualCare.ApiModels/HospitalInfo.cs
+++ SnapMD.VirtualCare.ApiModels/HospitalInfo.cs
@@ -1,42 +1,49 @@
-﻿using System.Collections.Generic;
+﻿#region Copyright
+//    Copyright 2016 SnapMD, Inc.
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//        http://www.apache.org/licenses/LICENSE-2.0
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+#endregion
+
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace SnapMD.VirtualCare.ApiModels
 {
-    public class HospitalInfo
+    public class HospitalInfo : ProviderBase
     {
-        public int HospitalId { get; set; }
-        public string HospitalName { get; set; }
-        public string ContactPerson { get; set; }
-        public string Email { get; set; }
-        public string HospitalImage { get; set; }
-        public string ContactNumber { get; set; }
-        public string IsActive { get; set; }
-
-        public string HospitalCode { get; set; }
-        public string BrandName { get; set; }
-        public string BrandTitle { get; set; }
-        public string Address { get; set; }
         public string City { get; set; }
-        public string State { get; set; }
-        public int? StateId { get; set; }
-        public double? ConsultationCharge { get; set; }
-        public string HospitalDomainName { get; set; }
-        public string BrandColor { get; set; }
-        public string ITDepartmentContactNumber { get; set; }
-        public string AppointmentsContactNumber { get; set; }
-        public string Locale { get; set; }
-        public string PatientLogin { get; set; }
-        public string PatientConsultEndUrl { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        [JsonConverter(typeof (StringEnumConverter))]
+        public CustomerSsoType CustomerSso { get; set; }
+
+        public string CustomerSsoLinkText { get; set; }
+
+        public List<string> EnabledModules { get; set; }
+
+        public int HospitalId { get; set; }
+
+        public string HospitalName { get; set; }
+
+        public string HospitalImage { get; set; }
+        public string IsActive { get; set; }
 
         public virtual IEnumerable<HospitalHours> OperatingHours { get; set; }
 
-        public List<string> EnabledModules { get; set; }
-        public IDictionary<string, string> Settings { get; set; }
+        public string HospitalDomainName { get; set; }
+        public string PatientConsultEndUrl { get; set; }
+        
+        public string PatientLogin { get; set; }
 
-        [JsonConverter(typeof(StringEnumConverter))]
-        public CustomerSsoType CustomerSso { get; set; }
-        public string CustomerSsoLinkText { get; set; }
+        public IDictionary<string, string> Settings { get; set; }
     }
 }
