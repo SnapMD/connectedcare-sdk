38ab7ab225f94a69a0e8a90ba01023218c7c7c84
Andrei <andrei.mikhalevich>
2016-02-24 14:09:50 -0400

    Appointment optimization code enum changed

--- SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentOptimizationCode.cs
+++ SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentOptimizationCode.cs
@@ -1,8 +1,18 @@
 ﻿namespace SnapMD.VirtualCare.ApiModels.Scheduling
 {
-    public enum AppointmentOptimizationCode : short
+    /// <summary>
+    /// Appointment optimization code.
+    /// </summary>
+    public enum AppointmentOptimizationCode
     {
+        /// <summary>
+        /// Single booking.
+        /// </summary>
         SingleBooking,
+
+        /// <summary>
+        /// Double booking.
+        /// </summary>
         DoubleBooking
     }
 }
