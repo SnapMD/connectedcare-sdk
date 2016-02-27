c8f7eb40e63393f534e514a6d89b3782d09b422c
Jay Lee <jay.lee@ji-technology.com>
2016-02-27 13:42:56 -0800

    add models for Staff Account Activation

---
+++ SnapMD.VirtualCare.ApiModels/UserActivationRequest.cs
@@ -0,0 +1,32 @@
+﻿namespace SnapMD.VirtualCare.ApiModels
+{
+    /// <summary>
+    /// Request Model for user activation.
+    /// </summary>
+    public class UserActivationRequest
+    {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        /// <value>
+        /// The token.
+        /// </value>
+        public string Token { get; set; }
+    }
+}
