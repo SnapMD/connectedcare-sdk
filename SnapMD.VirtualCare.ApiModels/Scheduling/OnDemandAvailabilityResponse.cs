82a9cd90f20bb500615a3f7720e4a55259073195
Jay Lee <jay.lee@ji-technology.com>
2016-03-15 15:14:22 -0700

    OnDemandAvailability models

---
+++ SnapMD.VirtualCare.ApiModels/Scheduling/OnDemandAvailabilityResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace SnapMD.VirtualCare.ApiModels.Scheduling
+{
+    /// <summary>
+    /// Response Model for OnDemandAvailability
+    /// </summary>
+    /// <seealso cref="SnapMD.VirtualCare.ApiModels.Scheduling.OnDemandAvailabilityRequest" />
+    public class OnDemandAvailabilityResponse : OnDemandAvailabilityRequest
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether [provider on demand].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [provider on demand]; otherwise, <c>false</c>.
+        /// </value>
+        public bool ProviderOnDemand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the on demand availability.
+        /// </summary>
+        /// <value>
+        /// The on demand availability.
+        /// </value>
+        public int OnDemandAvailability { get; set; }
+    }
+}
