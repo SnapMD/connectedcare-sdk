a7a51c9138ae10cd3c6b410338418cfd2f6b35cc
Jay Lee <jay.lee@ji-technology.com>
2016-03-16 14:49:53 -0700

    refactor: rename ondemandavailability fields

--- SnapMD.VirtualCare.ApiModels/Scheduling/OnDemandAvailabilityResponse.cs
+++ SnapMD.VirtualCare.ApiModels/Scheduling/OnDemandAvailabilityResponse.cs
@@ -7,19 +7,19 @@
     public class OnDemandAvailabilityResponse : OnDemandAvailabilityRequest
     {
         /// <summary>
-        /// Gets or sets a value indicating whether [provider on demand].
+        /// Gets or sets a value indicating whether [provider has ondemand enabled].
         /// </summary>
         /// <value>
-        ///   <c>true</c> if [provider on demand]; otherwise, <c>false</c>.
+        /// <c>true</c> if [provider has ondemand enabled]; otherwise, <c>false</c>.
         /// </value>
-        public bool ProviderOnDemand { get; set; }
+        public bool ProviderOnDemandEnabled { get; set; }
 
         /// <summary>
-        /// Gets or sets the on demand availability.
+        /// Gets or sets the count of ondemand availability blocks at the moment.
         /// </summary>
         /// <value>
-        /// The on demand availability.
+        /// The count of ondemand availability blocks at the moment.
         /// </value>
-        public int OnDemandAvailability { get; set; }
+        public int OnDemandAvailabilityBlockCount { get; set; }
     }
 }
