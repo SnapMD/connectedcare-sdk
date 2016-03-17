5956ea69f0ddf007b1f6377aa47aa6a38856d7a1
Andrei <andrei.mikhalevich>
2016-03-17 18:22:05 -0300

    Person and appointment model changes. ContactTypeCode moved to Api.Models

---
+++ SnapMD.VirtualCare.ApiModels/ContactTypeCode.cs
@@ -0,0 +1,33 @@
+﻿namespace SnapMD.VirtualCare.ApiModels
+{
+    /// <summary>
+    /// Person's contact type.
+    /// </summary>
+    public enum ContactTypeCode
+    {
+        /// <summary>
+        /// Contact (default).
+        /// </summary>
+        Contact,
+
+        /// <summary>
+        /// Patient.
+        /// </summary>
+        Patient,
+
+        /// <summary>
+        /// Practicioner.
+        /// </summary>
+        Practicioner,
+
+        /// <summary>
+        /// Hospital Admin.
+        /// </summary>
+        HospitalAdmin,
+
+        /// <summary>
+        /// Snap MD Staff.
+        /// </summary>
+        SnapMDStaff
+    }
+}
