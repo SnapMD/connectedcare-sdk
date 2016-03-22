a3c24be513d636343eaf55ba0219a4f55e1a403d
Aaron Lord <aaron@lorddesign.net>
2016-03-22 13:23:39 -0700

    Changes to support clinician list

--- SnapMD.VirtualCare.ApiModels/ClinicianPersonResponse.cs
+++ SnapMD.VirtualCare.ApiModels/ClinicianPersonResponse.cs
@@ -6,21 +6,13 @@ namespace SnapMD.VirtualCare.ApiModels
     public class ClinicianPersonResponse
     {
         /// <summary>
-        ///     Gets or sets the user identifier.
-        /// </summary>
-        /// <value>
-        ///     The user identifier.
-        /// </value>
-        public int UserId { get; set; }
-
-        /// <summary>
         ///     Gets or sets the person.
         /// </summary>
         /// <value>
         ///     The person.
         /// </value>
         public PersonRecord Person { get; set; }
-        
+
         /// <summary>
         ///     Gets or sets the provider identifier.
         /// </summary>
@@ -28,5 +20,21 @@ namespace SnapMD.VirtualCare.ApiModels
         ///     The provider identifier.
         /// </value>
         public int ProviderId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the specialty.
+        /// </summary>
+        /// <value>
+        ///     The specialty.
+        /// </value>
+        public ClinicianSpecialty Specialty { get; set; } = new ClinicianSpecialty();
+
+        /// <summary>
+        ///     Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        ///     The user identifier.
+        /// </value>
+        public int UserId { get; set; }
     }
 }
