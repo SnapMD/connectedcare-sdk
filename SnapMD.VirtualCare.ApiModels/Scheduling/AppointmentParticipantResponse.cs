10035a0b42b6d5da4f5533d847ffa864f7099c1d
Jay Lee <jay.lee@ji-technology.com>
2016-03-24 23:13:33 -0700

    AppointmentParticipantResponse: add UserId

--- SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentParticipantResponse.cs
+++ SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentParticipantResponse.cs
@@ -7,8 +7,28 @@ namespace SnapMD.VirtualCare.ApiModels.Scheduling
     /// </summary>
     public class AppointmentParticipantResponse : AppointmentParticipantRequest
     {
+        /// <summary>
+        /// Gets or sets the participant identifier.
+        /// </summary>
+        /// <value>
+        /// The participant identifier.
+        /// </value>
         public Guid ParticipantId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        public int? UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
         public virtual GlobalStatusCode Status { get; set; }
     }
 }
