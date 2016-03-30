66cdc5a7445992408ca33d80d5c9f8494229ee59
David Caballero <david.caballero@toptal.com>
2016-03-30 18:51:50 -0500

    re #5865 - adds apis for availability blocks

---
+++ SnapMD.VirtualCare.Sdk/Interfaces/IParticipantApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SnapMD.VirtualCare.ApiModels.Scheduling;
+using SnapMD.VirtualCare.Sdk.Models;
+
+namespace SnapMD.VirtualCare.Sdk.Interfaces
+{
+    public interface IParticipantApi
+    {
+        ApiResponseV2<AppointmentParticipantResponse> AddParticipant(Guid appointmentId, AppointmentParticipantRequest request);
+    }
+}
---
+++ SnapMD.VirtualCare.Sdk/ParticipantApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SnapMD.VirtualCare.ApiModels.Scheduling;
+using SnapMD.VirtualCare.Sdk.Interfaces;
+using SnapMD.VirtualCare.Sdk.Models;
+
+namespace SnapMD.VirtualCare.Sdk
+{
+    public class ParticipantApi : ApiCall, IParticipantApi
+    {
+        public ParticipantApi(string baseUrl, IWebClient client, string bearerToken = null, string developerId = null, string apiKey = null) : base(baseUrl, client, bearerToken, developerId, apiKey)
+        {
+        }
+
+        public ApiResponseV2<AppointmentParticipantResponse> AddParticipant(Guid appointmentId, AppointmentParticipantRequest request)
+        {
+            var result = Post<ApiResponseV2<AppointmentParticipantResponse>>
+                (
+                    $"v2.1/clinicians/appointments/{appointmentId}/participants",
+                    request
+                );
+
+            return result;
+        }
+    }
+}
