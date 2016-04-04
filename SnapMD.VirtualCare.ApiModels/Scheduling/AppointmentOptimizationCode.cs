dff2bb575899211388523acba3922a717c5940e9
Andrei <andrei.mikhalevich>
2016-04-04 18:06:43 -0300

    AppointmentOptimizationType inheritance removed

--- SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentOptimizationCode.cs
+++ SnapMD.VirtualCare.ApiModels/Scheduling/AppointmentOptimizationCode.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Appointment optimization code.
     /// </summary>
-    public enum AppointmentOptimizationCode : int
+    public enum AppointmentOptimizationCode
     {
         /// <summary>
         /// Single booking.
