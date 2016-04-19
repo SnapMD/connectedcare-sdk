cfd5ed0f73ddab4ed91c49520d3e77fe1e3e8e22
Andrei <andrei.mikhalevich>
2016-04-19 11:36:13 -0300

    Redesigning AvailabilityBlocks API models: Adding clinician, improving documentation.

--- SnapMD.VirtualCare.ApiModels/Scheduling/AvailabilityBlockClinicianResponse.cs
+++ SnapMD.VirtualCare.ApiModels/Scheduling/AvailabilityBlockClinicianResponse.cs
@@ -1,6 +1,35 @@
-﻿namespace SnapMD.VirtualCare.ApiModels.Scheduling
+﻿using System;
+
+namespace SnapMD.VirtualCare.ApiModels.Scheduling
 {
+    /// <summary>
+    /// Represents availability block clinician response.
+    /// </summary>
     public class AvailabilityBlockClinicianResponse : AvailabilityBlockClinicianRequest
     {
+        /// <summary>
+        /// Clinician availability block id.
+        /// </summary>
+        public Guid AvailabilityBlockId { get; set; }
+
+        /// <summary>
+        /// Created by user id.
+        /// </summary>
+        public int CreatedByUserId { get; set; }
+
+        /// <summary>
+        /// Modified by user id.
+        /// </summary>
+        public int? ModifiedByUserId { get; set; }
+
+        /// <summary>
+        /// Created date.
+        /// </summary>
+        public DateTimeOffset CreatedDate { get; set; }
+
+        /// <summary>
+        /// Last modified date.
+        /// </summary>
+        public DateTimeOffset? ModifiedDate { get; set; }
     }
 }
