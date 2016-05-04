fbd0fdce2da7e2b374649a74b940b42d113c6409
DavidC <david.caballero@snap.md>
2016-05-04 12:42:12 -0500

    re #4908 - adds api methods to personapi

--- SnapMD.VirtualCare.Sdk/Interfaces/IPersonApi.cs
+++ SnapMD.VirtualCare.Sdk/Interfaces/IPersonApi.cs
@@ -6,5 +6,10 @@ namespace SnapMD.VirtualCare.Sdk.Interfaces
     public interface IPersonApi
     {
         ApiResponseV2<PersonRecord> GetPersonByEmail(string email, int? userType = null);
+
+        ApiResponseV2<ClinicianPersonResponse> GetClinicianPerson(int providerId, int clinicianUserId);
+
+        ApiResponseV2<PatientPersonResponse> GetPatientPerson(int providerId, int patientId);
+
     }
 }
