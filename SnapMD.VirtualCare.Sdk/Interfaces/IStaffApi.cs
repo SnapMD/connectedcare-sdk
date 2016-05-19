acbd9eeae8d9d063285aac4c6d3f1616a55cfc8e
Aaron Lord <aaron@lorddesign.net>
2016-05-19 10:08:51 -0700

    Working on async improvements

--- SnapMD.VirtualCare.Sdk/Interfaces/IStaffApi.cs
+++ SnapMD.VirtualCare.Sdk/Interfaces/IStaffApi.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SnapMD.VirtualCare.ApiModels;
-using SnapMD.VirtualCare.Sdk.Models;
+﻿using SnapMD.VirtualCare.ApiModels;
 
 namespace SnapMD.VirtualCare.Sdk.Interfaces
 {
