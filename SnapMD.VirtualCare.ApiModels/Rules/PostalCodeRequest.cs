9758ea97fefb2e0407b230c300a1a0b3b79bdbaa
Jay Lee <jay.lee@ji-technology.com>
2016-06-06 17:51:24 -0700

    postal search

---
+++ SnapMD.VirtualCare.ApiModels/Rules/PostalCodeRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SnapMD.VirtualCare.ApiModels.Rules
+{
+    /// <summary>
+    /// Request Model for PostalCode requests
+    /// </summary>
+    /// <seealso cref="SnapMD.VirtualCare.ApiModels.Rules.SubjectAddress" />
+    public class PostalCodeRequest : SubjectAddress
+    {
+        /// <summary>
+        /// The state abbreviation code.
+        /// </summary>
+        /// <value>
+        /// The state code.
+        /// </value>
+        public string StateCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postal code identifier.
+        /// </summary>
+        /// <value>
+        /// The postal code identifier.
+        /// </value>
+        public Guid? PostalCodeId { get; set; }
+
+        /// <summary>
+        /// Partial text of state, city or postal code for search.
+        /// </summary>
+        /// <value>
+        /// The search text.
+        /// </value>
+        public string SearchText { get; set; }
+
+        /// <summary>
+        /// The maximum rows should be returned from the search results.
+        /// </summary>
+        /// <value>
+        /// The maximum rows.
+        /// </value>
+        public int? MaxRows { get; set; }
+    }
+}
