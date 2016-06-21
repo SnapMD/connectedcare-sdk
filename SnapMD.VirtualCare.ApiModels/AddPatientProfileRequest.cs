f62e6a08e8136d07e88362d566205c711ba74b1c
Aaron Lord <aaron@lorddesign.net>
2016-06-21 16:59:11 -0700

    Import models for patient/dependent insert

---
+++ SnapMD.VirtualCare.ApiModels/AddPatientProfileRequest.cs
@@ -0,0 +1,18 @@
+﻿using SnapMD.Web.Api.Models;
+
+namespace SnapMD.VirtualCare.ApiModels
+{
+    /// <summary>
+    /// The API uses JSON camel case serialization, so the variables supplied in the incoming request will have lower case.
+    /// </summary>
+    public class AddPatientProfileRequest
+    {
+        public string EmailAddress { get; set; }
+        public int TimeZoneId { get; set; }
+        public PatientUpdateRequest PatientProfileData { get; set; }
+        public LegacyMedicalHistoryForm PatientMedicalHistoryData { get; set; }
+        public PatientProfileFieldChangesTrackingRequest PatientProfileFieldsTracing { get; set; }
+    }
+
+ 
+}
---
+++ SnapMD.VirtualCare.ApiModels/PatientUpdateRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SnapMD.VirtualCare.ApiModels
+{
+    public class PatientUpdateRequest
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Dob { get; set; }
+        public int? BloodType { get; set; }
+        public int? EyeColor { get; set; }
+        public string Gender { get; set; }
+        public int? Ethnicity { get; set; }
+        public int? HairColor { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string SchoolName { get; set; }
+        public string SchoolContact { get; set; }
+        public string PrimaryPhysician { get; set; }
+        public string PrimaryPhysicianContact { get; set; }
+        public string PhysicianSpecialist { get; set; }
+        public string PhysicianSpecialistContact { get; set; }
+        public string PreferedPharmacy { get; set; }
+        public string PharmacyContact { get; set; }
+        public string Address { get; set; }
+        public string ProfileImagePath { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public int? HeightUnit { get; set; }
+        public int? WeightUnit { get; set; }
+        public string Organization { get; set; }
+        public string Location { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? LocationId { get; set; }
+    }
+}
