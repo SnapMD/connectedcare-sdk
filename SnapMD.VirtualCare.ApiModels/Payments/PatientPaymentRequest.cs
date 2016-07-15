15a12a27fdfa9fe0da2b95f235975d4448584291
Jay Lee <jay.lee@ji-technology.com>
2016-07-15 16:44:26 -0700

    patientPayment models

---
+++ SnapMD.VirtualCare.ApiModels/Payments/PatientPaymentRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SnapMD.VirtualCare.ApiModels.Payments
+{
+    /// <summary>
+    /// Request Model for PatientPayment API
+    /// </summary>
+    public class PatientPaymentRequest
+    {
+        /// <summary>
+        /// The patient payment identifier.
+        /// </summary>
+        public Guid? PatientPaymentId { get; set; }
+
+        /// <summary>
+        /// The credit amount.
+        /// </summary>
+        public decimal? CreditAmount { get; set; }
+
+        /// <summary>
+        /// The debit amount.
+        /// </summary>
+        public decimal? DebitAmount { get; set; }
+    }
+}
