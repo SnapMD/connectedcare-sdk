df3a6803316beb317366660de98f00c818ffcddb
Jay Lee <jay.lee@ji-technology.com>
2016-07-21 15:05:50 -0700

    patientpayments

---
+++ SnapMD.VirtualCare.ApiModels/Payments/PatientPaymentResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SnapMD.VirtualCare.ApiModels.Scheduling;
+
+namespace SnapMD.VirtualCare.ApiModels.Payments
+{
+    /// <summary>
+    /// Response Model for PatientPaymentRequest
+    /// </summary>
+    /// <seealso cref="SnapMD.VirtualCare.ApiModels.Payments.PatientPaymentRequest" />
+    public class PatientPaymentResponse : PatientPaymentRequest
+    {
+        /// <summary>
+        /// Gets or sets the patient identifier.
+        /// </summary>
+        /// <value>
+        /// The patient identifier.
+        /// </value>
+        public int PatientId { get; set; }
+
+        /// <summary>
+        /// The credit date.
+        /// </summary>
+        public DateTime CreditDate { get; set; }
+
+        /// <summary>
+        /// The debit date.
+        /// </summary>
+        public DateTime? DebitDate { get; set; }
+
+        /// <summary>
+        /// The appointment type code.
+        /// </summary>
+        public AppointmentTypeCode AppointmentTypeCode { get; set; }
+    }
+}
