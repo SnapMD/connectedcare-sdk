204e27e21c8181c9eb87f3ae45a99efa5b6fbb66
Aaron Lord <aaron@lorddesign.net>
2016-08-03 11:44:59 -0700

    cm

--- SnapMD.VirtualCare.ApiModels/Properties/AssemblyInfo.cs
+++ SnapMD.VirtualCare.ApiModels/Properties/AssemblyInfo.cs
@@ -26,16 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2016 SnapMD, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("7fb09fbc-00e7-41a0-a334-c6fa3564f206")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
