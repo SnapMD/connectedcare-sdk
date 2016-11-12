63050990df0f3e8cd18d46641b11b2e62357cc43
eparfinenko <eparfinenko@slcode.com>
2016-11-12 18:39:15 +0300

    Add MeetingId to ConsultationParticipantRequest

--- SnapMD.VirtualCare.ApiModels/ConsultationParticipantResponse.cs
+++ SnapMD.VirtualCare.ApiModels/ConsultationParticipantResponse.cs
@@ -5,5 +5,10 @@ namespace SnapMD.VirtualCare.ApiModels
     public class ConsultationParticipantResponse: ConsultationParticipantRequest
     {
         public Guid PersonId { get; set; }
+        
+        /// <summary>
+        /// The id of consultation meeting (chat)
+        /// </summary>
+        public Guid? MeetingId { get; set; }
     }
 }
