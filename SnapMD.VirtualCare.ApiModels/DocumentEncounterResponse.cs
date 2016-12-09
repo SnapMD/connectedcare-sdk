395061f0b09a8065b9e15cd0d134799d5ed902dc
dhawal <dhawalh@outlook.com>
2016-12-09 10:18:12 +0530

    Added DocumentEnconterRequest and Reponse.

---
+++ SnapMD.VirtualCare.ApiModels/DocumentEncounterResponse.cs
@@ -0,0 +1,23 @@
+﻿using SnapMD.VirtualCare.ApiModels.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapMD.VirtualCare.ApiModels
+{
+    /// <summary>
+    /// DocumentEncounterResponse
+    /// </summary>
+    public class DocumentEncounterResponse : DocumentEncounterRequest
+    {
+        //TODO: May be not required. Revisit this
+        //public new List<AppointmentParticipantResponse> Participants { get; set; }
+
+        /// <summary>
+        /// ConsultationId
+        /// </summary>
+        public int? ConsultationId { get; set; }
+    }
+}
