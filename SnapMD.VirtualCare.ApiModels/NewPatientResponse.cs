920d78ecf9a90451d26cd62b683eb3a36f9713d2
mostafiz57 <mostafiz57@gmail.com>
2018-11-27 22:38:37 +0600

    SnapMD#13971 , remove sonarqube bugs

--- SnapMD.VirtualCare.ApiModels/NewPatientResponse.cs
+++ SnapMD.VirtualCare.ApiModels/NewPatientResponse.cs
@@ -13,28 +13,102 @@ using System;
 
 namespace SnapMD.VirtualCare.ApiModels
 {
+    /// <summary>
+    /// Represents New Patient Response.
+    /// </summary>
     public class NewPatientResponse
     {
+        /// <summary>
+        ///     Get or Set the Address.
+        /// </summary>
+        /// <value>
+        ///    Address
+        /// </value>
         public string Address { get; set; }
-
+        /// <summary>
+        ///     Get or Set AddressObject.
+        /// </summary>
+        /// <value>
+        ///    Line 1
+        ///    Line 2
+        ///    City 
+        ///    State
+        ///    StateCode
+        ///    PostalCode
+        ///    Country
+        ///    CountryCode
+        ///    AddressText
+        /// </value>
         public AddressObject AddressObject { get; set; }
-
+        /// <summary>
+        ///     Get or Set the Date of Birth.
+        /// </summary>
+        /// <value>
+        ///    Date of Birth
+        /// </value>
         public DateTime? Dob { get; set; }
-        
+        /// <summary>
+        ///     Get or Set the Time Zone Id.
+        /// </summary>
+        /// <value>
+        ///   TimeZoneId
+        /// </value>
+
         public int TimeZoneId { get; set; }
+        /// <summary>
+        ///     Get or Set the Email.
+        /// </summary>
+        /// <value>
+        ///   Email
+        /// </value>
 
         public string Email { get; set; }
+        /// <summary>
+        ///     Get or Set the Name.
+        /// </summary>
+        /// <value>
+        ///   Name
+        /// </value>
 
         public FirstLast Name { get; set; }
+        /// <summary>
+        ///     Get or Set the Patient Id.
+        /// </summary>
+        /// <value>
+        ///   PatientId
+        /// </value>
 
         public int PatientId { get; set; }
+        /// <summary>
+        ///     Get or Set the Provider Id.
+        /// </summary>
+        /// <value>
+        ///   ProviderId
+        /// </value>
 
         public int ProviderId { get; set; }
+        /// <summary>
+        ///     Get or Set the UserLoginId.
+        /// </summary>
+        /// <value>
+        ///   UserLoginId
+        /// </value>
 
         public int UserLoginId { get; set; }
+        /// <summary>
+        ///     Get or Set the Gender.
+        /// </summary>
+        /// <value>
+        ///   Male or Female
+        /// </value>
 
         public string Gender { get; set; }
-
+        /// <summary>
+        ///     Get or Set the MobilePhone.
+        /// </summary>
+        /// <value>
+        ///  MobilePhone
+        /// </value>
         public string MobilePhone { get; set; }
     }
 }
--- SnapMD.VirtualCare.ApiModels/PatientRecord.cs
+++ SnapMD.VirtualCare.ApiModels/PatientRecord.cs
@@ -2,24 +2,90 @@
 
 namespace SnapMD.VirtualCare.ApiModels
 {
+    /// <summary>
+    /// Represents Patient Record.
+    /// </summary>
     public class PatientRecord
     {
+        /// <summary>
+        /// Get or set the Height
+        /// </summary>
+        /// <value>
+        /// Height
+        /// </value>
         public string Height { get; set; }
 
+        /// <summary>
+        /// Get or set the Weight
+        /// </summary>
+        /// <value>
+        /// Weight
+        /// </value>
         public string Weight { get; set; }
 
+        /// <summary>
+        /// Get or set the HeightUnit
+        /// </summary>
+        /// <value>
+        /// HeightUnit
+        /// </value>
         public string HeightUnit { get; set; }
 
+        /// <summary>
+        /// Get or set the WeightUnit
+        /// </summary>
+        /// <value>
+        /// WeightUnit
+        /// </value>
         public string WeightUnit { get; set; }
 
+        /// <summary>
+        /// Get or set the Address
+        /// </summary>
+        /// <value>
+        /// Address
+        /// </value>
         public string Address { get; set; }
 
+        /// <summary>
+        /// Get or set the AddressObject
+        /// </summary>
+        /// <value>
+        ///    Line 1
+        ///    Line 2
+        ///    City 
+        ///    State
+        ///    StateCode
+        ///    PostalCode
+        ///    Country
+        ///    CountryCode
+        ///    AddressText
+        /// </value>
         public AddressObject AddressObject { get; set; }
 
+        /// <summary>
+        /// Get or set the HomePhone
+        /// </summary>
+        /// <value>
+        /// HomePhone
+        /// </value>
         public string HomePhone { get; set; }
 
+        /// <summary>
+        /// Get or set the MobilePhone
+        /// </summary>
+        /// <value>
+        /// MobilePhone
+        /// </value>
         public string MobilePhone { get; set; }
 
+
+        /// <summary>
+        /// Get or set the Dob
+        /// </summary>
+        /// <value>
+        /// Dob
+        /// </value>
         public DateTime? Dob { get; set; }
 
         /// <summary>
@@ -30,14 +96,44 @@ namespace SnapMD.VirtualCare.ApiModels
         /// </value>
         public string Gender { get; set; }
 
+        /// <summary>
+        /// Get or set the Organization Name
+        /// </summary>
+        /// <value>
+        /// OrganizationName
+        /// </value>
         public string OrganizationName { get; set; }
 
+        /// <summary>
+        /// Get or set the Location Name
+        /// </summary>
+        /// <value>
+        /// LocationName
+        /// </value>
         public string LocationName { get; set; }
 
+        /// <summary>
+        /// Get or set the FirstName
+        /// </summary>
+        /// <value>
+        /// FirstName
+        /// </value>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Get or set the LastName
+        /// </summary>
+        /// <value>
+        /// LastName
+        /// </value>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Get or set the Profile Image Path
+        /// </summary>
+        /// <value>
+        /// ProfileImagePath
+        /// </value>
         public string ProfileImagePath { get; set; }
     }
 }
