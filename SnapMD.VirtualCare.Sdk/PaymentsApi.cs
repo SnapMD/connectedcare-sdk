066c68cfb102fe18c1e1336adfbfa7346651db65
mostafiz57 <mostafiz57@gmail.com>
2018-12-03 18:19:18 +0600

    SanpMD#14355 , remove sonarqube bugs

--- SnapMD.VirtualCare.Sdk/PaymentsApi.cs
+++ SnapMD.VirtualCare.Sdk/PaymentsApi.cs
@@ -16,22 +16,50 @@ using SnapMD.VirtualCare.Sdk.Interfaces;
 
 namespace SnapMD.VirtualCare.Sdk
 {
+    /// <summary>
+    /// Represents Payments Api.
+    /// </summary>
     public class PaymentsApi : ApiCall
     {
+        /// <summary>
+        /// Constructor of PaymentsApi.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="baseUrl"></param>
+        /// <param name="bearerToken"></param>
+        /// <param name="developerId"></param>
+        /// <param name="hospitalId"></param>
+        /// <param name="webClient"></param>
         public PaymentsApi(string baseUrl, string bearerToken, int hospitalId, string developerId, string apiKey, IWebClient webClient)
             : base(baseUrl, webClient, bearerToken, developerId, apiKey)
         {
             HospitalId = hospitalId;
         }
 
+        /// <summary>
+        /// Constructor of PaymentsApi.
+        /// </summary>
+        /// <param name="baseUrl"></param>
+        /// <param name="hospitalId"></param>
+        /// <param name="webClient"></param>
         public PaymentsApi(string baseUrl, int hospitalId, IWebClient webClient)
             : base(baseUrl, webClient)
         {
             HospitalId = hospitalId;
         }
 
+        /// <summary>
+        ///     Get or Set HospitalId .
+        /// </summary>
+        /// <value>
+        ///    HospitalId
+        /// </value> 
         public int HospitalId { get; private set; }
 
+        /// <summary>
+        ///     Get the Customer Profile .
+        /// </summary>
+        /// <param name="patientUserId"></param>
         public ApiResponseV2<CimCustomer> GetCustomerProfile(int? patientUserId)
         {
             //fixed to match the unit test
@@ -39,12 +67,20 @@ namespace SnapMD.VirtualCare.Sdk
             return result;
         }
 
+        /// <summary>
+        ///     Get the Register Profile .
+        /// </summary>
+        /// <param name="paymentData"></param>
         public ApiResponseV2<PaymentProfilePostResult> RegisterProfile(object paymentData)
         {
             var result = Post<ApiResponseV2<PaymentProfilePostResult>>(string.Format("v2/patients/payments"), paymentData);
             return result;
         }
 
+        /// <summary>
+        ///     Get the Get Payment Status .
+        /// </summary>
+        /// <param name="consultationId"></param>
         public ApiResponseV2<bool> GetPaymentStatus(int consultationId)
         {
             var result = MakeCall<ApiResponseV2<bool>>(string.Format("v2/patients/copay/{0}/paymentstatus", consultationId));
