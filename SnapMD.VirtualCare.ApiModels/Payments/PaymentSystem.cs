50caac0d416d3ee35d071c09b4ccb4f9709e6a4c
andrey.antsiporovich <andrey.antsiporovich@gmail.com>
2021-02-04 00:50:34 +0300

    #18755: Changes for PaymentWall integration

---
+++ SnapMD.VirtualCare.ApiModels/Payments/PaymentSystem.cs
@@ -0,0 +1,20 @@
+﻿using SnapMD.VirtualCare.ApiModels.Attributes;
+
+namespace SnapMD.VirtualCare.ApiModels.Payments
+{
+    [ModelName("ApiModels.PaymentSystem")]
+    public class PaymentSystem
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool NewWindow { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public string ImgClass { get; set; }
+
+        public string PsTypeId { get; set; }
+    }
+}
